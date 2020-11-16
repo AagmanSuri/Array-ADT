5b7a97f19edafd00b95bc93538746a674847a6ec
Aagman Suri <aagmansuri96@gmail.com>
2020-11-16 21:37:51 +0530

    Initial Commit

---
+++ Array ADT/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Array ADT
+//
+//  Created by Aagman Suri on 16/11/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
