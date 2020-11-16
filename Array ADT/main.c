7cbd2031b52729983fb7bb79425dac04c0c2687f
Aagman Suri <aagmansuri96@gmail.com>
2020-11-16 21:41:01 +0530

    Update main.c

--- Array ADT/main.c
+++ Array ADT/main.c
@@ -1,14 +1,47 @@
-//
-//  main.c
-//  Array ADT
-//
-//  Created by Aagman Suri on 16/11/20.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+struct Array
+{
+    int *A;
+    int size;
+    int length ;
+};
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int Display(struct Array t)
+{
+    for(int i =0;i<t.length;i++)
+    {
+        printf("%d",t.A[i]);
+    }
     return 0;
 }
+int main()
+{   int n,i ;
+    struct Array arr;
+    printf("Enter the size of an array");
+    scanf("%d",&arr.size);
+    arr.A= (int *) malloc(arr.size*sizeof(int));
+    arr.length =0;
+    
+    printf("enter the number of number : ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr.A[i]);
+    }
+    arr.length=n;
+    
+    Display(arr);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
