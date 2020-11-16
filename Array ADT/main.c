8f13b860839ba127e3c22b97055e2f1639769784
Aagman Suri <aagmansuri96@gmail.com>
2020-11-16 22:09:51 +0530

    Update main.c

--- Array ADT/main.c
+++ Array ADT/main.c
@@ -16,10 +16,26 @@ void Display(struct Array arr)
         printf("%d ",arr.A[i]);
     }
 }
+//append(x)
+void Append(struct Array *arr,int x)
+{
+    if(arr->length<arr->size)
+    {
+        arr->A[arr->length]=x;
+        arr->length++;
+    }
+}
+//insert(index,x)
+//delete(index)
+//search
+//linear search
+//Binary search
+
 int main()
 {
     struct Array arr={{2,3,4,5,6},20,5};
     
+    Append(&arr, 7);
     Display(arr);
     
     return 0;
