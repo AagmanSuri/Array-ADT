b661dd83bca64b06327933317bc97dd2b08b20a2
Aagman Suri <aagmansuri96@gmail.com>
2020-11-19 16:40:13 +0530

    Difference

--- Array ADT/main.c
+++ Array ADT/main.c
@@ -334,6 +334,36 @@ struct Array * Intersection(struct Array *arr1 ,struct Array *arr2)
     
     return arr3;
 }
+struct Array * Difference(struct Array *arr1 ,struct Array *arr2)
+{   int i,j,k;
+    i=j=k=0;
+    struct Array *arr3=(struct Array *)malloc(sizeof(struct Array));
+    while(i<arr1->length && j<arr2->length)
+    {   //one smaller
+        if(arr1->A[i]<arr2->A[j])
+        {
+            arr3->A[k++]= arr1->A[i++];
+        }
+        else if(arr2->A[j]<arr1->A[i])
+        {
+            j++;
+        }
+        // if both are equal copy anyone and increment the other
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+    for(;i<arr1->length;i++)
+    {
+        arr3->A[k++]=arr1->A[i++];
+    }
+    
+    arr3->length =k;
+    arr3->size=10;
+    return arr3;
+}
 int main()
 {
     //struct Array arr={{-2,3,-4,5,6},10,5};
@@ -361,7 +391,8 @@ int main()
     //Rearrange(&arr);
     //arr3= Merging(&arr1, &arr2);
     //arr3= Union(&arr1, &arr2);
-    arr3= Intersection(&arr1, &arr2);
+    //arr3= Intersection(&arr1, &arr2);
+    arr3= Difference(&arr1, &arr2);
     Display(*arr3);
     
     return 0;
